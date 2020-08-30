940b978e29fc1ef523703eb2f7285471f2ff2bda
Aaron Denny <aaron.denny@wayne.edu>
2020-08-30 16:16:11 -0400

    binding to custom properties

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+  /**
+   * We define a JavaScript object that we want to receive from the app component.
+   * 
+   * By default, all properties of components are only accessible inside of their components, not from
+   * outside.
+   * 
+   * If you want to allow parent components to be able to bind to a property, you must add the @Input
+   * decorator
+   */
+  @Input() element: {
+    type: string,
+    name: string,
+    content: string
+  };
 
   constructor() { }
 
